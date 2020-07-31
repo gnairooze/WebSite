3f859cfe92a9d71cc118dc594f3fbd9669627277
George Nairooze <gnairooze@gmail.com>
2020-07-31 14:48:42 +0200

    first commit

---
+++ Basics/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics
+{
+    public class Enums
+    {
+        public enum Status { 
+            NotSet = 0,
+            Succeeded = 1,
+            Failed = 2,
+            Skipped = 4,
+            Canceled = 8,
+            Processing = 16,
+            Stalled = 32
+        }
+    }
+}
---
+++ Basics/Result.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Basics
+{
+    public class Result
+    {
+        public Enums.Status ResultStatus { get; set; }
+        public int Code { get; set; }
+        public string Description { get; set; }
+        public JObject Data { get; set; }
+    }
+}
